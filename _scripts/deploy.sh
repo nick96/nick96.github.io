5d2486345cb5431b633267ec0331d58415a5ccad
nick96 <nick96@bots.github.com>
2020-12-22 20:21:52 +1100

    Don't deploy with gh actions, just use script manually

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
-set -euo xtrace
+set -eu
 
-repo_uri="https://x-access-token:${DEPLOY_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_name="origin"
 main_branch="master"
 target_branch="gh-pages"
 build_dir="_output"
-
-cd "$GITHUB_WORKSPACE"
-
-git config user.name "$GITHUB_ACTOR"
-git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
 make -B _clean _build
 git add "$build_dir"
@@ -21,12 +15,11 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-git remote set-url "$remote_name" "$repo_uri"
-
 subtree_head=$(git subtree split --prefix "$build_dir" "$main_branch")
 if [ -z "$subtree_head"]
 then
     echo "Failed to get subtree head"
     exit 1
 fi
+
 git push "$remote_name" "$subtree_head":"$target_branch" --force-with-lease
